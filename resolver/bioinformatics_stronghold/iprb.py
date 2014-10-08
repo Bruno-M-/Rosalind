087f6986cbf225bc0c6f8e2359e5db00498e7a99
Bruno Maitre <bruno@MacBook-Pro-de-Bruno.local>
2014-10-08 15:00:06 +0200

    Add iprb resolver and testsuite

---
+++ resolver/bioinformatics_stronghold/iprb.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+import rosalind
+
+try:
+	file_name = sys.argv[1]
+except:
+	print "Error "+sys.argv[0]+" no input file"
+	exit(1)
+
+try: 
+	fd = open(file_name, 'r')
+except:
+	print "Error ["+sys.argv[0]+"] Input ("+file_name+") must be a valid file"
+	exit(1)
+
+sample = fd.readline()
+k, m, n = sample.split()
+
+print rosalind.iprb(float(k), float(m), float(n))
+
